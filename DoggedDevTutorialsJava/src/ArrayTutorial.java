ddef9e6d139fa87f5389a91cf4ad712e9d95b3c5
rjdrexel <richdrexel@gmail.com>
2016-09-01 13:42:03 -0400

    test commit

---
+++ DoggedDevTutorialsJava/src/ArrayTutorial.java
@@ -0,0 +1,20 @@
+
+/**
+ * 
+ * @author Richard Drexel
+ * @since 2016-09-01
+ * @version 1
+ * 
+ * <h1>Array Tutorial for <a href="http://www.doggeddev.com/">DoggedDev</a></h1>
+ * 
+ *
+ */
+public class ArrayTutorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
